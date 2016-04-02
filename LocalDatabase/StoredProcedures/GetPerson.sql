373935e5183fa5db1040f481bd6ba2b512f32238
christo8989 <christo8989@gmail.com>
2016-04-02 16:23:25 -0700

    Added database project for testing

---
+++ LocalDatabase/StoredProcedures/GetPerson.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetPerson]
+	@personId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		[PersonId], 
+		[FirstName], 
+		[LastName], 
+		[Age], 
+		[Height], 
+		[Weight] 
+	FROM [dbo].[Person] 
+	WHERE [PersonId] = @personId
+
+	RETURN 0
+END
---
+++ LocalDatabase/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[PersonId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Age] INT NOT NULL, 
+    [Height] INT NULL, 
+    [Weight] INT NULL
+)
